719f387a0d29581d6e2ba1dd3733064cebe67cc3
syelurilyft <46057208+syelurilyft@users.noreply.github.com>
2019-11-04 13:21:49 -0800

    Delete Dashboard_Upload.bat

--- Dashboard_Upload.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd d:\Tableau\Tableau Server\10.5\bin
-
-tabcmd login  -s http://localhost -u Admin -p tableauadmin
-tabcmd publish "C:\Users\Administrator\Downloads\DAX Spend Report - Q4.twbx" r- Channel Health -o
-
-@echo Done
-
