a2ab78ea8d692627531601d83deb55a79b6ea48e
syelurilyft <46057208+syelurilyft@users.noreply.github.com>
2019-11-04 13:25:08 -0800

    Delete once.bat

--- once.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd d:\Tableau\Tableau Server\10.5\bin
-
-tabcmd publish "C:\Users\Administrator\Downloads\Business Health 17Q4-test.twb"  -s http://localhost:9417 -U eroberts -p lyftslalomtest
-
-@echo Done
-
